384e635344002af356dcdcb9ca1c1240cc1b9af5
Thato Botsane <thbjr@outlook.com>
2024-02-25 19:10:08 +0200

    Completes project section (no css)

--- thbjr/src/Components/Projects/Project.jsx
+++ thbjr/src/Components/Projects/Project.jsx
@@ -1,11 +1,34 @@
+/* eslint-disable react/prop-types */
+
+import { Link } from "react-router-dom"
+
 const Project = () => {
   return (
-    <div>
-      
+    <div className="project-container">
+      <ProjectTile/>
+      <ProjectTile/>
+      <ProjectTile/>
     </div>
   )
 }
 
 export default Project
 
+function ProjectTile() {
+  return (
+    <div className="project">
 
+      <div className="optionsholder">
+      <ProjectOption className={"btn-code"} text={"Source Code"}/>
+      <ProjectOption className={"btn-live"} text={"Live Preview"}/>
+      </div>
+
+    </div>
+  )
+}
+
+function ProjectOption({ className, text }) {
+  return (
+    <Link className={className}>{text}</Link>
+  )
+}
